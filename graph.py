c67795710305b1e62f980ed9d595a56e87b3d79d
RSTeja <52061352+RSTeja@users.noreply.github.com>
2023-03-23 14:03:52 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,78 +1,57 @@
-from typing import Dict, List, Set
-
+import codecs
 
 class Graph:
-    def __init__(self, vertices: List[int], num_colors: int, edges: Dict[int, Set[int]]):
+    def __init__(self, vertices):
         self.vertices = vertices
-        self.num_colors = num_colors
-        self.edges = edges
+        self.adj_list = {v: set() for v in vertices}
+        self.coloring = {}
 
+    def add_edge(self, u, v):
+        self.adj_list[u].add(v)
+        self.adj_list[v].add(u)
 
-class CSP:
-    def __init__(self, graph: Graph):
-        self.graph = graph
-        self.domains = {v: set(range(graph.num_colors)) for v in graph.vertices}
-        self.constraints = {v: set() for v in graph.vertices}
-        for v1, v2 in graph.edges.items():
-            self.constraints[v1].add(v2)
-            self.constraints[v2].add(v1)
+    def get_unassigned_variable(self):
+        unassigned = [v for v in self.vertices if v not in self.coloring]
+        if not unassigned:
+            return None
+        return min(unassigned, key=lambda v: len(self.adj_list[v]))
 
-    def solve(self):
-        assignment = {}
-        return self.backtrack(assignment)
-
-    def backtrack(self, assignment):
-        if len(assignment) == len(self.graph.vertices):
-            return assignment
-
-        var = self.select_unassigned_variable(assignment)
-        for value in self.order_domain_values(var, assignment):
-            if self.is_consistent(var, value, assignment):
-                assignment[var] = value
-                result = self.backtrack(assignment)
-                if result is not None:
-                    return result
-                del assignment[var]
-        return None
-
-    def select_unassigned_variable(self, assignment):
-        unassigned_vars = [v for v in self.graph.vertices if v not in assignment]
-        return self.most_constrained_variable(unassigned_vars)
-
-    def order_domain_values(self, var, assignment):
-        return list(self.domains[var])
-
-    def is_consistent(self, var, value, assignment):
-        for neighbor in self.constraints[var]:
-            if neighbor in assignment and assignment[neighbor] == value:
+    def is_valid_assignment(self, variable, value):
+        for neighbor in self.adj_list[variable]:
+            if neighbor in self.coloring and self.coloring[neighbor] == value:
                 return False
         return True
 
-    def most_constrained_variable(self, variables):
-        return min(variables, key=lambda v: len(self.domains[v]))
+    def csp_backtracking(self):
+        variable = self.get_unassigned_variable()
+        if variable is None:
+            return True
+        domain = list(range(1, num_colors + 1))
+        domain.sort(key=lambda v: sum(self.is_valid_assignment(variable, v) for variable in self.adj_list))
+        for value in domain:
+            if self.is_valid_assignment(variable, value):
+                self.coloring[variable] = value
+                if self.csp_backtracking():
+                    return True
+                del self.coloring[variable]
+        return False
 
 
-def read_input_file(file_path: str) -> Graph:
-    with open(file_path, 'r', encoding='utf-8-sig') as file:  # use 'utf-8-sig' encoding to remove BOM
-        num_colors = int(file.readline().strip())
-        edges = {}
-        for line in file:
-            v1, v2 = map(int, line.strip().split(','))
-            if v1 not in edges:
-                edges[v1] = set()
-            if v2 not in edges:
-                edges[v2] = set()
-            edges[v1].add(v2)
-            edges[v2].add(v1)
-        vertices = sorted(edges.keys())
-    return Graph(vertices, num_colors, edges)
+# Read input file
+filename = '/input2.txt'
 
+with codecs.open(filename, 'r', encoding='utf-8-sig') as f:
+    num_colors = int(f.readline().strip())
+    vertices = [tuple(map(int, line.strip().split(','))) for line in f]
 
-if __name__ == '__main__':
-    graph = read_input_file('input.txt')
-    csp = CSP(graph)
-    solution = csp.solve()
-    if solution is not None:
-        print('Solution:', solution)
-    else:
-        print('No solution found')
+# Create graph and add edges
+g = Graph(set(v for e in vertices for v in e))
+for u, v in vertices:
+    g.add_edge(u, v)
+
+# Solve using CSP and MRV heuristic
+if g.csp_backtracking():
+    for v, color in g.coloring.items():
+        print(f"Vertex {v} is assigned color {color}")
+else:
+    print("No solution found.")
